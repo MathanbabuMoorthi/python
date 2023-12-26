12468eb6bd696989d8e504a701e42432ee3bf2d6
MathanbabuMoorthi <154584669+MathanbabuMoorthi@users.noreply.github.com>
2023-12-26 13:57:41 +0530

    Update name.py

--- name.py
+++ name.py
@@ -1,6 +1,6 @@
 
 def personal_details():
-    name, age = "Simon", 19
+    name, age = "Simon", 24
     address = "Bangalore, Karnataka, India"    
     print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
 personal_details()
